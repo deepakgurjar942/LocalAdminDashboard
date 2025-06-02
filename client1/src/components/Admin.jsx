74e699e8909bcb45830d8dbfecde7ee093dd4644
unknown <deepakg932>
2025-06-02 11:39:14 +0530

    add

--- client1/src/components/Admin.jsx
+++ client1/src/components/Admin.jsx
@@ -1,11 +1,8 @@
 import React from 'react'
-import { BrowserRouter,Router,Route } from 'react-router-dom'
-import Dashboard from './pages/Dashboard'
-import App from '../App'
+
 const Admin = () => {
   return (
-
-App
+    <div>Admin</div>
   )
 }
 
